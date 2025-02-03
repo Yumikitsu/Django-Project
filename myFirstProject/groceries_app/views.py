1845fd003e3bd69ee0602403fbf023c227576329
Joaquin Lindkvist <joaquinzache@gmail.com>
2025-02-03 15:28:51 +0100

    Basic Grocery App is up and running

--- myFirstProject/groceries_app/views.py
+++ myFirstProject/groceries_app/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def app_homepage(request):
-    return HttpResponse("This is the newly created app")
+    return render(request, 'homepage.html')
+
+def about_us(request):
+    return render(request, "aboutUs.html")
+
+def services(request):
+    return render(request, "services.html")
+
+def contact_us(request):
+    return render(request, "contactUs.html")
