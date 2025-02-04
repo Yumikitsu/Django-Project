d049f32be0025f2392b89451ec9d8f1229af899f
Joaquin Lindkvist <joaquinzache@gmail.com>
2025-02-04 07:37:07 +0100

    Added register, sign in and logged in pages.

--- myFirstProject/groceries_app/models.py
+++ myFirstProject/groceries_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class RegisteredUser(models.Model):
+    name = models.CharField(max_length=100)
+    emailid = models.CharField(max_length=100)
+    phoneNum = models.CharField(blank=True, null=True, max_length=20)
+    password = models.CharField(max_length=30)
